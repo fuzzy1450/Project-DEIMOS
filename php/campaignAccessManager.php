ff02eb61f923fe8aefd0a4bbd70355fa6ebfe4ae
Evan Kupsch <ievank@me.com>
2018-10-22 14:46:17 -0400

    Added two functions to campaignAccessManager.php
    +grantCampaignAccess(): function to give a user access to a campaign
    
    +revokeCampaignAccess(): function to take away a user's access to a campaign

--- php/campaignAccessManager.php
+++ php/campaignAccessManager.php
@@ -59,9 +59,7 @@ function getCampaignDetails($id){
 	include "mySQLVariables.php";
 	$conn = new mysqli($sqlServer, $sqlUsername, $sqlPassword, $dbname);
 	if (mysqli_connect_errno()) {
-		$result = false;
-		$errors = 'Failed to connect to login database';
-		die(json_encode(array("success"=>$result, "message"=>$errors)));
+		return [false, "Failed to connect to database"];
 	}
 
 	$stmt = $conn->prepare("SELECT * FROM `campaigns` WHERE ID=?");
@@ -70,5 +68,93 @@ function getCampaignDetails($id){
 	return $stmt->get_result()->fetch_array();
 }
 
+/**
+ *  Grant a user access to a campaign given a user ID and a campaign ID
+ *  
+ *  @param int $UserID Internal ID of the user you want access given to
+ *  @param int $CampaignID Internal ID of the campaign you want to give access to
+ *  
+ *  @return mixed[] An array that contains a boolean and a string. The boolean will be true if the opperation completed successfully. The string will be empty unless the opperation fails, in which case it will contain a reason for failure.
+ */
+function grantCampaignAccess($UserID, $CampaignID){
+	if(hasCampaignAccess($CampaignID, $UserID)){
+		return [false, "User Already has access to this campaign"];
+	}
+	
+	// Get array of campaigns user already has access to
+	$userCampaignList = getCampaignsList($UserID);
+	
+	// Add the campaign id to the list of campaigns that the user already has access to
+	array_push($userCampaignList, $CampaignID);
+	
+	// Update the database with the new list
+	include "mySQLVariables.php";
+	$conn = new mysqli($sqlServer, $sqlUsername, $sqlPassword, $dbname);
+	if (mysqli_connect_errno()) {
+		return [false, "Failed to connect to database"];
+	}
+
+	$stmt = $conn->prepare("UPDATE `users` SET `AccessedCampaigns`=? WHERE ID=?");
+	$UserCampaignJSON = json_encode($userCampaignList);
+	$stmt->bind_param("si", $UserCampaignJSON, $UserID);
+	$stmt->execute();
+	
+	$QueryResult = $stmt->get_result();
+	
+	if($stmt->affected_rows == 1){
+		return [true, ""];
+	} else {
+		return [false, "Something went wrong: stmt->affected_rows != 1"];
+	}
+}
+
+
+/**
+ *  Revokes a user's access to a campaign given a user ID and a campaign ID
+ *  
+ *  @param int $UserID Internal ID of the user you want to revoke access from
+ *  @param int $CampaignID Internal ID of the campaign who's access is to be revoked
+ *  
+ *  @return mixed[] An array that contains a boolean and a string. The boolean will be true if the opperation completed successfully. The string will be empty unless the opperation fails, in which case it will contain a reason for failure.
+ */
+function revokeCampaignAccess($UserID, $CampaignID){ //TODO: properly test this function
+	if(!hasCampaignAccess($CampaignID, $UserID)){
+		return [false, "User already does not have access to this campaign"];
+	}
+	
+	// Get array of campaigns user already has access to
+	$userCampaignList = getCampaignsList($UserID);
+	
+	
+	// Remove the id from the user's list
+	$i = 0;
+	foreach($userCampaignList as $Camp_id){
+		if($Camp_id == $CampaignID){
+			array_splice($userCampaignList, $i, 1);
+		}
+		$i++;
+	}
+	
+	// Update the database with the new list
+	include "mySQLVariables.php";
+	$conn = new mysqli($sqlServer, $sqlUsername, $sqlPassword, $dbname);
+	if (mysqli_connect_errno()) {
+		return [false, "Failed to connect to database"];
+	}
+
+	$stmt = $conn->prepare("UPDATE `users` SET `AccessedCampaigns`=? WHERE ID=?");
+	$UserCampaignJSON = json_encode($userCampaignList);
+	$stmt->bind_param("si", $UserCampaignJSON, $UserID);
+	$stmt->execute();
+	
+	$QueryResult = $stmt->get_result();
+	
+	if($stmt->affected_rows == 1){
+		return [true, ""];
+	} else {
+		return [false, "Something went wrong: stmt->affected_rows != 1"];
+	}
+	
+}
 
 ?>
